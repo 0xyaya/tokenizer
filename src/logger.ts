bd282662c5f3cfca937cb9f1f5e622f9a4675550
yann <yann.thibault1@gmail.com>
2024-02-01 21:31:11 +0100

    cNFT

---
+++ src/logger.ts
@@ -0,0 +1,10 @@
+export function logger(originalMethod: any, _context: any) {
+    function replacementMethod(this: any, ...args: any[]) {
+        console.log('start:', originalMethod.name);
+        const result = originalMethod.call(this, ...args);
+        console.log('end:', originalMethod.name);
+        return result;
+    }
+
+    return replacementMethod;
+}
